e5249ba90d04a62d5a339ea3480504e50f29c439
IsaccBarker <milobanks@rowlandhall.org>
2023-04-23 01:32:04 -0600

    emacs revamp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,106 +1,143 @@
 (defun do-pretty ()
     (set-frame-font (concat font " " (number-to-string fontsize)) nil t)
-    ;; Load theme of the week
-    (require 'kaolin-themes)
-    (load-theme 'kaolin-dark t)
     ;; Disable menu bar, scroll bar, and tool bar
     (menu-bar-mode -1)
     (scroll-bar-mode -1)
     (tool-bar-mode -1)
-    ;; Display extra whitespace at the end of lines
-	(add-hook 'prog-mode-hook
-		(if (not (string= (frame-parameter nil 'name) "terminal")) (setq show-trailing-whitespace t)))
-    ;;(setq-default show-trailing-whitespace t)
     ;; Add a line at column 100
     (setq-default display-fill-column-indicator-column 100)
-    (global-display-fill-column-indicator-mode 1))
+    (global-display-fill-column-indicator-mode 1)
+    ;; Change window size and position
+    (add-to-list 'default-frame-alist '(height . 48))
+    (add-to-list 'default-frame-alist '(width . 120))
+    (set-frame-position (selected-frame) 20 40)
+    ;; Disable line numbers in certain modes but make them the default otherwise
+    (column-number-mode)
+    (global-display-line-numbers-mode t)
+    (dolist (mode '(term-mode-hook vterm-mode-hook eshell-mode-hook))
+      (add-hook mode (lambda () (display-line-numbers-mode 0))))
+    ;; Disable evil mode in terminals and other places I don't want it. Evil mode has already been
+    ;; set as the default in other places.
+    (dolist (mode '(term-mode vterm-mode eshell-mode))
+      (evil-set-initial-state mode 'emacs))
+    ;; Disable showing extra whitespace at the end of lines in terminals but make it the
+    ;; default otherwise.
+    (setq-default show-trailing-whitespace t)
+    (dolist (mode '(term-mode-hook vterm-mode-hook eshell-mode-hook))
+        (add-hook mode (lambda ()
+            (make-local-variable 'show-trailing-whitespace)
+            (setq show-trailing-whitespace nil)))))
 
-(defun do-qol()
-    ;; Tab behaviour I actually want
-    (setq-default indent-tabs-mode nil)
-    (setq-default tab-width 4)
-    (setq-default indent-line-function 'insert-tab)
-    ;; Open ~/Developer if it exists, otherwise go to the home directory
-    (cond ((file-directory-p "~/Developer") (setq initial-buffer-choice "~/Developer"))
-          (t (setq initial-buffer-choice "~")))
-    (setq compile-command "make -j"))
+(defun setup-shell ()
+    ;; Emacs's default shell is Bash, we want ZSH
+    (setq zsh (executable-find "zsh"))
+    (setq-default shell-file-name zsh)
+    (setq-default explicit-shell-file-name zsh))
 
-;; START PACKAGE SETUP ZONE
-;; All packages should have a matching function named "setup-<package_name>". It will be
-;; automatically run (Lisp is neat!). Time will tell if this is a good idea or not.
+(defun setup-path ()
+    ;; Make the emacs PATH variable match that of ZSHs.
+    (interactive)
+    (setq path-from-shell (shell-command-to-string (concat zsh " --login -c 'echo $PATH'")))
+    (setq exec-path (split-string path-from-shell path-separator))
+    (setenv "PATH" path-from-shell))
 
-(defun setup-evil ()
-    (evil-mode 1)
-	(evil-ex-define-cmd "refresh-packages" 'refresh-packages))
+(defun setup-dashboard ()
+    (setq dashboard-banner-logo-title
+        "\"Emacs may stand for 'Editor MACroS', but with MyMacs, it stands for 'Extremely Marvelous And Convenient Stuff'!\"")
+    (setq dashboard-startup-banner "~/.emacs.d/welcome.txt")
+    (setq dashboard-center-content t)
+    (setq dashboard-show-shortcuts nil)
+    (setq dashboard-items '((recents  . 3)
+                        (projects . 3)))
+    (dashboard-setup-startup-hook))
 
-;;(Defunsetup-centaur-tabs ()
-;;    (centaur-tabs-mode t)
-;;    (centaur-tabs-headline-match)
-;;    (centaur-tabs-change-fonts font (* fontsize 10)) ;; Need to multiply by 10 for some reason.
-;;    (setq centaur-tabs-set-icons t)
-;;    (setq centaur-tabs-style "bar")
-;;    (setq centaur-tabs-set-modified-marker t)
-;;    (setq centaur-tabs-modified-marker "•"))
+(defun setup-projectile ()
+    (require 'projectile)
+    (when (file-directory-p "~/Developer")
+        (setq projectile-project-search-path '("~/Developer")))
+    (setq projectile-switch-project-action #'projectile-dired)
+    (projectile-mode +1)
+    (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
 
-(defun setup-vterm ()
-  (setq-default vterm-shell (executable-find "zsh")))
-
-(defun setup-highlight-quoted() (add-hook 'emacs-lisp-mode-hook 'highlight-quoted-mode))
-(defun setup-highlight-defined() (add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode))
-(defun setup-highlight-numbers() (add-hook 'prog-mode-hook 'highlight-numbers-mode))
-(defun setup-kaolin-themes ())
-(defun setup-all-the-icons ())
-
-;; END PACKAGE SETUP ZONE
-
-(defun setup-package-archive ()
-	(require 'package)
-	(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-	(package-initialize)
-	(package-refresh-contents))
-
-(defun get-packages ()
-    (dolist (package packages)
-        (when (not (package-installed-p package))
-            (package-install package))))
+(defun setup-ivy ()
+    (ivy-mode)
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t))
 
 (defun setup-all-packages ()
-    ;; Run any package setup function that might exist.
-    (dolist (package packages)
-        (require package)
-        (setq setup-func-name (concat "setup-" (symbol-name package)))
-        (setq setup-func (intern setup-func-name))
-        (if (not (boundp setup-func)) (funcall setup-func))))
+    ;; Org mode
+    (use-package org)
+    (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
-(defun refresh-packages ()
-	(interactive)
-	(setup-package-archive)
-	(get-packages)
-    (setup-all-packages))
+    ;; Vim-like modal editing
+    (straight-use-package '(evil :host github :repo "emacs-evil/evil"))
+    (use-package evil
+        :config (evil-mode t))
+
+    ;; Virtual terminal
+    (straight-use-package 'vterm)
+    (use-package vterm
+        :config (setq-default vterm-shell (executable-find "zsh")))
+
+    ;; ThemeofTheDay
+    (straight-use-package 'kaolin-themes)
+    (use-package kaolin-themes
+        :config (load-theme 'kaolin-dark t))
+
+    ;; A dashboard that I'll configure to strip down
+    (straight-use-package 'dashboard)
+    (use-package dashboard
+        :config (setup-dashboard))
+
+    ;; Project management solution
+    (straight-use-package 'projectile)
+    (use-package projectile
+        :diminish projectile-mode
+        :config (setup-projectile))
+
+    ;; Keybindings options panel
+    ;; I can't memorize everything from the start
+    (straight-use-package 'which-key)
+    (use-package which-key
+        :diminish which-key-mode
+        :init (which-key-mode)
+        :config (setq which-key-idle-delay 1))
+
+    ;; Completion mechanism
+    (straight-use-package 'ivy)
+    (use-package ivy
+        :config (setup-ivy))
+
+    ;; Nice highlighting
+    (straight-use-package 'highlight-quoted)
+    (use-package highlight-quoted)
+    (straight-use-package 'highlight-defined)
+    (use-package highlight-defined)
+    (straight-use-package 'highlight-numbers)
+    (use-package highlight-numbers))
 
 (defun main ()
-  (setq-default packages (list 'evil 'kaolin-themes 'all-the-icons 'highlight-quoted
+    (setup-shell)
+    (setup-path)
+    (defvar bootstrap-version)
+    (let ((bootstrap-file
+           (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+          (bootstrap-version 6))
+      (unless (file-exists-p bootstrap-file)
+        (with-current-buffer
+            (url-retrieve-synchronously
+             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+             'silent 'inhibit-cookies)
+          (goto-char (point-max))
+          (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+    (setq-default packages (list 'evil 'kaolin-themes 'highlight-quoted
                                'highlight-defined 'highlight-numbers 'vterm))
+    (setq package-enable-at-startup nil)
+    (defvar bootstrap-version)
     (setq-default font "Ubuntu Mono")
     (setq-default fontsize 16)
-	;; To setup and get new packages, use the refresh-packages command I've provided
-	(setup-all-packages)
-    (do-qol)
-	(do-pretty))
+    (setup-all-packages)
+    (do-pretty))
 
 (main)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vterm multi-compile all-the-icons highlight-numbers highlight-number highlight-defined highlight-quoted highlight-quotes kaolin-themes solarized-theme centaur-tabs monokai-theme evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
